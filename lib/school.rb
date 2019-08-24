2b2d5b56751480ceba7020015392307d479ed3c5
Jharell Chambers <jharell.chambers@selu.edu>
2019-08-24 01:46:30 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School 
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student_name, student_grade)
+    @roster[student_grade] ||= []
+    @roster[student_grade] << student_name
+  end 
+  
+  def grade(student_grade)
+   return @roster[student_grade]
+  end
+  
+  def sort 
+    student_sort = {}
+    @roster.each do |student_grade, students|
+      student_sort[student_grade] = students.sort 
+    end
+    
+    return student_sort
+  end 
+end
+    
