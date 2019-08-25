a80647bae615a201ac56cd6d5df94c5fa6a37a71
Mars Le <hp.mars.le@gmail.com>
2019-08-25 04:01:22 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+  
+  attr_accessor :roster
+  
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end 
+  
+  def add_student(student_name, grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << student_name
+    else 
+      @roster[grade] = []
+      @roster[grade] << student_name
+    end 
+  end 
+  
+  def grade(nth_grade)
+    @roster[nth_grade]
+  end 
+  
+  def sort
+    @roster.each{|k,v|@roster[k]=v.sort}
+    @roster
+  end 
+  
+  
+end 
+
+# school = School.new("Bayside High School")
