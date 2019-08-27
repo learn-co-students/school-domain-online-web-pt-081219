9bbb168b1c24c0a605025786da0e5de9cf1b15fa
imperative-genre-4923 <dreama.lin@yale.edu>
2019-08-27 22:08:27 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School
+  attr_accessor :grade, :length
+  attr_reader :roster, :sort, :name
+
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.sort.to_h.each { |k, v|
+      v.sort!
+    }
+  end
+end
