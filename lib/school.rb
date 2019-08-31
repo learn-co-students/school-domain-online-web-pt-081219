e4dc509cbb7d6a3ccb30b8b7767ae1a150653acc
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-08-31 02:14:19 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+
+class School
+  attr_accessor :name, :roster
+   def initialize(name)
+    @name = name  
+    @roster = {}
+   end
+   def add_student(student, gr) 
+     roster[gr] ||= []
+     roster[gr] << student
+   end
+   def grade(gr)
+    roster.detect do |x, y| 
+      if x == gr
+        return y 
+      end 
+  end 
+end
+def sort 
+  new = {}
+  roster.each do |x, y| 
+    new[x] = y.sort 
+  end 
+  new
+end 
+end
