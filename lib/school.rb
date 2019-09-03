b818e6e2b4f62600db6c0be6aea0d9e6edd4d1e5
Kevin Hayden <kevin.fhayden@gmail.com>
2019-09-03 04:25:30 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,41 @@
-# code here!
+class School
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def roster
+    @roster
+  end
+  
+  def add_student(name, grade)
+    if @roster[grade]
+      if !(@roster[grade].include?(name))
+        @roster[grade] << name
+      end
+    else
+      @roster[grade] = []
+      @roster[grade] << name
+    end
+  end
+  
+  def grade(year)
+    @roster.detect do |a, b|
+      if a == year
+        return b
+      end
+    end
+  end
+  
+  def sort
+    sorted_roster = {}
+    @roster.each do |a, b|
+      sorted_roster[a] = b.sort
+    end
+    sorted_roster
+  end
+  
+  school = School.new("Bayside High School")
+  
+end
