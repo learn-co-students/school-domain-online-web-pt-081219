b3efec4c6026cc7e44d395a93554e3983afdf7b6
Jeffrey Brown <brownjs03@gmail.com>
2019-09-18 01:45:46 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+class School
+  @@roster = {}
+  
+  def initialize(name)
+    @name = name 
+    @@roster
+  end 
+  
+  def roster
+    @@roster = {}
+  end 
+  
+  def add_student(name, grade)
+    @name = name
+    @grade = grade 
+  end 
+
