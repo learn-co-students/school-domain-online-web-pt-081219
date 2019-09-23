b9cebaf6204203e7ea0e01caabdc9ab674cd2802
LUIS J Alvarez Diaz <ldiaz244@gmail.com>
2019-09-23 23:46:49 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,6 @@
-# code here!
+school = School.new("Bayside High School")
+school.roster
+school.add_student("Zack Morris", 9)
+school.roster
+hash ={}
+hash["new_key"] << "new-value_for_value_array"
